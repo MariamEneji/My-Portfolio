dd2740a21f3e87ad71f8f18a844326ae7b85a5ea
Mariam Eneji <enejimariam1997@gmail.com>
2023-06-03 16:57:58 +0100

    last commit?

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 import Head from 'next/head'
-import Main from './components/Main'
-import Navbar from './components/Navbar'
-import About from './components/About'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
+import Main from '../components/Main'
+import Navbar from '../components/Navbar'
+import About from '../components/About'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import Contact from '../components/Contact'
 
 
 
